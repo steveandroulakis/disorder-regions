c7b19897123ee022d9725a6bdb56de9dc9a96338
Steve Androulakis <steve.androulakis@gmail.com>
2012-02-28 19:10:54 +1100

    Fixed highlighting bug in sequences where there'd be a repeating character

--- disorder_regions/disorder/shortcuts.py
+++ disorder_regions/disorder/shortcuts.py
@@ -1,9 +1,9 @@
 def highlight_region(seq, start, end):
     highlight_start = '<span class="sequence_highlight">'
     highlight_end = '</span>'
-    end = end + len(highlight_start)
-    seq_region = seq[:start] + highlight_start + seq[start-1:]
-    seq_region = seq_region[:end+1] + highlight_end + seq_region[end:]
+    end = end + len(highlight_start)-1
+    seq_region = seq[:start-1] + highlight_start + seq[start-1:]
+    seq_region = seq_region[:end+1] + highlight_end + seq_region[end+1:]
     return str(seq_region)
     
 def highlight_mutation(seq, position, mutated_aa):
